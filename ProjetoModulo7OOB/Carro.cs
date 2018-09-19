cb783ffeb9b920ce5a5c8fcd51746b1bee986331
Lucasedu191 <lucasatletico@hotmail.com>
2018-09-18 23:50:26 -0300

    Conceito basico de orientação a objeto criando uma classe sobre carro

---
+++ ProjetoModulo7OOB/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProjetoModulo7OOB
+{
+    public class Carro
+    {
+        public String cor;
+        public String modelo;
+        public double velocidadeMaxima;
+        public int qtdRodas;
+        public int qtdPortas;
+
+        public string Ligar()
+        {
+            return "Carro Ligado";
+        }
+        public string Acelerar()
+        {
+            return "Carro acelerou";
+        }
+    }
+}
--- ProjetoModulo7OOB/Form1.cs
+++ ProjetoModulo7OOB/Form1.cs
@@ -12,9 +12,32 @@ namespace ProjetoModulo7OOB
 {
     public partial class Form1 : Form
     {
+        private Carro meuCarro;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+            
+            meuCarro = new Carro();
+            meuCarro.cor = "Vermelho";
+            meuCarro.modelo = "Ferrari";
+            meuCarro.qtdPortas = 2;
+            meuCarro.qtdRodas = 4;
+            meuCarro.velocidadeMaxima = 160;
+        }
+
+        private void btnAcelerar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(meuCarro.Acelerar());
+        }
+
+        private void btnLigar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(meuCarro.Ligar());
         }
     }
 }
